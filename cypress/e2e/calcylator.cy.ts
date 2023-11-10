106f3cce7e3f727a8f365652c83b8da0b9d1a113
THEOD0R1 <squirrelnub@gmail.com>
2023-11-10 16:02:54 +0100

    started with testing addition

--- cypress/e2e/calcylator.cy.ts
+++ cypress/e2e/calcylator.cy.ts
@@ -1,5 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("template spec", () => {
+  beforeEach("testing conection", () => {
+    cy.visit("");
+  });
+  it("Finde element", () => {
+    cy.get("div#app");
+  });
+  it("Submit form and calc", () => {
+    cy.get("input#num1").type("4");
+    cy.get("input#num2").type("6");
+    cy.get("select#operator").select("divi");
+  });
+
+  it("Test input values", () => {
+    cy.get("button").click();
+  });
+});
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,6 +1,26 @@
 import "./../scss/main.scss";
 
-function calculator(num1:number, num2:number, operator:sum) {
-    return operator(num1, num2);
+function calculator(num1: number, num2: number, operator: number) {
+  return operator(num1, num2);
 }
 
+const calcInput = (
+  document.getElementById("calcInput") as HTMLElement
+).addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("hej");
+
+  add();
+});
+
+const add = () => {
+  const userInput1 = (document.getElementById("num1") as HTMLInputElement)
+    .value;
+  const userInput2 = (document.getElementById("num2") as HTMLInputElement)
+    .value;
+
+  const inputResult = Number(userInput1) + Number(userInput2);
+
+  const result = document.getElementById("resultText");
+  result?.innerHTML = inputResult;
+};
